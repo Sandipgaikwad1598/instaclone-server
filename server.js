cd867c1acccff0ddaf656ce32e88f2e44843f6bb
Sandipgaikwad1598 <sandipgaikwad1598@gmail.com>
2022-07-22 11:15:07 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,75 @@
+ 
+const express= require ("express")
+const mongoose = require("mongoose");
+const multer=require("multer")
+const postModel = require("./model/post")
+const app=express()
+const fs=require("fs")
+const cors = require("cors");
+const url="http://localhost:3000";
+
+//middleware
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+//storage
+const Storage=multer.diskStorage({
+    destination:"uploads",
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname)
+    }
+})
+
+const upload=multer({
+    storage:Storage
+}).single("testimage")
+
+const port = process.env.PORT || 3001;
+
+mongoose.connect("mongodb://localhost/inst",()=>{
+    console.log("coonect to db...");
+},(err)=>{
+    console.log(err);
+})
+
+
+app.post("/post",(req,res)=>{
+    upload(req,res,(err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            const newImage=new postModel({
+                author:req.body.name,
+                location:req.body.location,
+                description:req.body.description,
+                date:req.body.date,
+                image:{
+                    data:fs.readFileSync("uploads/" +req.file.filename) ,
+                    contentType:"image/png"
+                }
+            })
+            newImage.save().then(()=>{
+                res.send("success")
+            }).catch((err)=>{
+                console.log(err)
+            })
+        }
+    })
+})
+
+
+app.get("/", (req,res)=>{
+    try{
+        postModel.find().sort({_id:-1}).then((allData)=>{
+            
+            res.status(200).send(allData)
+        })
+    }catch(err){
+        console.log(err)
+    }
+})
+app.listen(port, function() {
+    console.log("Server started.......on",port);
+});
